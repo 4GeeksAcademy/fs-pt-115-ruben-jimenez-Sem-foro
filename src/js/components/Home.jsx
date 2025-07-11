ef080e9024babf9517f20d73c755ba7c95334306
RUB3N02 <rubyerizo@gmail.com>
2025-07-11 16:13:50 +0000

    Le falta poco

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,28 @@
-import React from "react";
 
+import React, { useState } from "react";
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+import { Semaforo } from "./Semaforo"
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+const Home = () => {
+	const botones = [
+		{ id: 1, color: "green" },
+		{ id: 2, color: "yellow" },
+		{ id: 3, color: "red" },
+	];
+
+	const botonesMorados = [...botones, { id: 4, color: "purple" }];
+
+	const [botonesActivos, setBotonesActivos] = useState(true)
+	const resultado = botonesActivos ? botones : botonesMorados;
+
+
+
+	return (
+		<div className="d-flex flex-column justify-content-center align-items-center min-vh-100 gap-4">
+			<Semaforo botones={resultado}></Semaforo>
+
+			<button id="añade" className="btn " onClick={() => setBotonesActivos(!botonesActivos)}>Añade morado</button>
 		</div>
 	);
 };
---
+++ src/js/components/Semaforo.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+export const Semaforo = ({ botones }) => {
+
+    const [activo, setActivo] = useState(null);
+
+    return (
+        <div className="d-flex justify-content-center align-items-center gap-2">
+            <div id="cuerpoSemaforo" className="d-flex flex-column justify-content-center align-items-center gap-4" >
+
+                {botones.map((botones, index) => (
+                    <button type="button"
+                        className="btn rounded-circle fs-2"
+                        key={index}
+                        onClick={() => setActivo(activo === index ? null : index)}
+                        style={{
+                            backgroundColor: `${botones.color}`,
+                            width: "70px",
+                            height: "70px",
+                            boxShadow: activo === index ? "0 0 15px 5px rgba(235, 228, 228, 0.5)" : "none"
+                        }}
+                    ></button>
+                ))}
+            </div>
+        </div >
+    )
+}
